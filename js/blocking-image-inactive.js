572c779708f81fc27b3d2a49ec1514c54d55bfa1
DaniloSanchez <dasmdanilo@gmail.com>
2014-01-14 11:12:23 -0600

    refactor serInterval function

--- js/blocking-image-inactive.js
+++ js/blocking-image-inactive.js
@@ -2,30 +2,44 @@ var timeoutToActivateImage = 100;
     windowRemainsOnTop = true;
     windowsOnTop = true;
 
-window.onfocus = function () { 
-  windowsOnTop = true; 
-};
+window.onfocus = function () {
+  windowsOnTop = true;
+}
 
-window.onblur = function () { 
-  windowsOnTop = false; 
-};
+window.onblur = function () {
+  windowsOnTop = false;
+}
 
-setInterval(function () { 
-  if (window.windowsOnTop){
-    if (windowRemainsOnTop) {
-      timeoutToActivateImage = 100;
-      windowRemainsOnTop = true;
-    }else{
-      timeoutToActivateImage = 1000;
-      sleep(500);
-      $("#mainPhoto").attr('src', ACTIVE_IMAGE);
-      windowRemainsOnTop=true;
-    }
-  }else{
-    $("#mainPhoto").attr('src', INACTIVE_IMAGE);
-    windowRemainsOnTop = false;
-  }
+setInterval(function () {
+  if (window.windowsOnTop)
+    windowsIsActive();
+  else
+    changeToInactiveImage();
 }, timeoutToActivateImage);
+
+function windowsIsActive(){
+  if (windowRemainsOnTop)
+    changeTimeoutToActivateImage();
+  else
+    changeToActiveImage();
+}
+
+function changeTimeoutToActivateImage(){
+  timeoutToActivateImage = 100;
+  windowRemainsOnTop = true;
+}
+
+function changeToInactiveImage(){
+  $("#mainPhoto").attr('src', INACTIVE_IMAGE);
+  windowRemainsOnTop = false;
+}
+
+function changeToActiveImage() {
+  timeoutToActivateImage = 1000;
+  sleep(500);
+  $("#mainPhoto").attr('src', ACTIVE_IMAGE);
+  windowRemainsOnTop=true;
+}
 
 function sleep(ms) {
   var start = new Date().getTime(), expire = start + ms;
